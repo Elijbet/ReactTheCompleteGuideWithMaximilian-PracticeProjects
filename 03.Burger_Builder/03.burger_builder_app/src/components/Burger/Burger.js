e8ff9223e14e5d491bc6ac76372c8e7921b2f5cb
Eliza Khachatryan <elijbet@gmail.com>
2019-04-28 11:50:39 -0700

    Outputting burger ingredients dynamically by first parsing through an object to make array of ingredients containing an array of items to represent the number of each in the burger.

--- 03.Burger_Builder/03.burger_builder_app/src/components/Burger/Burger.js
+++ 03.Burger_Builder/03.burger_builder_app/src/components/Burger/Burger.js
@@ -3,14 +3,20 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+	const transformedIngredients = Object.keys(props.ingredients)
+		.map(igKey => {
+			//[...Array(3)] will return an array with 3 empty elements
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey}/>;
+			});
+		});
 	return(
 		<div className={classes.Burger}>
 			<BurgerIngredient type="bread-top"/>
-			<BurgerIngredient type="chesse"/>
-			<BurgerIngredient type="meat"/>
+				{transformedIngredients}
 			<BurgerIngredient type="bread-bottom"/>
 		</div>
-	);
+	)
 }
 
 export default burger;
