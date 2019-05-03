1316abac91d7e44adcf5c455856de17c1ab0ac91
Eliza Khachatryan <elijbet@gmail.com>
2019-05-02 20:40:17 -0700

    how to undo last commit containing node_modules, clear cache and gitignore properly. Rendering Fetched Data to the screen.

--- 04.HTTP_Requests/http--01-starting-setup/README.md
+++ 04.HTTP_Requests/http--01-starting-setup/README.md
@@ -10,3 +10,17 @@
 * Understanding the project and introducing axios.
 * Creating HTTP requests to get data.
 > There is one lifecycle hook that should be used for side-effects: componentDidMount(). But not for updating states since it triggers a rerender, unless that is what you want.
+
+Sidenote: The file from the class didn't have node modules in the gitignore. Here are the steps to undo the commited node modules for future reference, since we don't want to push these to Github. 
+
+Undo last commit:
+`git reset --soft HEAD~1`
+
+How to remove cached node_modules:
+#add 'node_modules' to .gitignore file
+
+`git rm -r --cached node_modules`
+`git commit -m 'Remove the now ignored directory node_modules'`
+`git push origin master`
+
+* Rendering Fetched Data to the screen.
--- 04.HTTP_Requests/http--01-starting-setup/src/containers/Blog/Blog.js
+++ 04.HTTP_Requests/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -7,22 +7,27 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state={
+        posts: []
+    }
     componentDidMount(){
         //you can't save this in a variable directly, since requests are
         //asynchronous and will block the execution of the next line.
         //Use .then
         axios.get('http://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                console.log('response', response)
+                this.setState({posts: response.data})
+                //console.log('response', response)
             })
     }
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title}/>;
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
